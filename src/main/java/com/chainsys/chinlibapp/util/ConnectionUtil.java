e1b7bc6d1eb7c4ae5c130677759fa62b4e51ddee
Chinraj M <chinraj044@gmail.com>
2020-03-26 09:57:06 +0530

    con

--- src/main/java/com/chainsys/chinlibapp/util/ConnectionUtil.java
+++ src/main/java/com/chainsys/chinlibapp/util/ConnectionUtil.java
@@ -3,6 +3,8 @@ package com.chainsys.chinlibapp.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.TimeZone;
+
 import com.chainsys.chinlibapp.exception.DbException;
 
 public class ConnectionUtil {
@@ -10,7 +12,12 @@ public class ConnectionUtil {
 		Connection connection = null;
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-			connection = DriverManager.getConnection("jdbc:oracle:thin:@Localhost:1521:XE", "system", "oracle");
+
+			String server = "13.235.147.120";// "localhost";
+			TimeZone timeZone = TimeZone.getTimeZone("Asia/Kolkata");
+			TimeZone.setDefault(timeZone);
+
+			connection = DriverManager.getConnection("jdbc:oracle:thin:@" + server + ":1521:XE", "chinraj", "chinraj");
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		}
